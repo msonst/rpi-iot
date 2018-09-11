8396e9e4d6f7d8030e25307d80ecfd03b2e7431b
Michael Sonst <codedorado@gmail.com>
2018-09-10 22:31:40 -0400

    Fixes docker-compose not found issue

--- rpi-iot-server/server-install.sh
+++ rpi-iot-server/server-install.sh
@@ -33,7 +33,7 @@ sudo gpasswd -a $USER docker
 # Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Start Docker Compose
 docker-compose up
