ed1e6ff36f7cfa468f3d68d3e3be004ee00c6d2d
Michael Sonst <codedorado@gmail.com>
2018-09-10 22:25:15 -0400

    Fixes issue with newgrp during installation

--- rpi-iot-server/server-install.sh
+++ rpi-iot-server/server-install.sh
@@ -28,7 +28,7 @@ sudo apt-get update
 curl -fsSL get.docker.com -o get-docker.sh && sh get-docker.sh
 sudo groupadd docker
 sudo gpasswd -a $USER docker
-newgrp docker
+
 
 # Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
